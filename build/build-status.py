6b6045b49446e8ac477be1de490836e1a848e23b
Tingzhi Li <vincentltz@gmail.com>
2015-05-17 04:34:57 -0700

    update

--- build/build-status.py
+++ build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3-dev-topoTestAdhoc-optimized']
+ns3_runnable_programs = ['build/scratch/ns3-dev-topoTestAdhoc-optimized', 'build/scratch/ns3-dev-topoTest-optimized']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
