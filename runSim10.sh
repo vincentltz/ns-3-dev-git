7a412b0be458bcb447580046aaa3534d5126f41b
Tingzhi Li <vincentltz@gmail.com>
2015-05-22 20:30:12 -0700

    add runSim10

---
+++ runSim10.sh
@@ -0,0 +1,6 @@
+
+FILE=./scratch/Net_10
+
+	echo $f
+	#./waf
+	./waf --run "topoTestAdhoc --infile="$FILE" --outHopsFile=scratch/maxhopscmd_1.txt --outTimeFile=scratch/maxtimecmd_1.txt --outAvgMsgFile=scratch/avgmsgcmd_1.txt"
