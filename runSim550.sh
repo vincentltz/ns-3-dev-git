c123d4cf4acadce07149a0021da60b7d10e308e7
Tingzhi Li <vincentltz@gmail.com>
2015-05-22 23:30:01 -0700

    add more files

---
+++ runSim550.sh
@@ -0,0 +1,10 @@
+
+FILES=./scratch/Net_550_*
+
+for f in $FILES
+do
+
+	echo $f
+	#./waf
+	./waf --run "topoTestAdhoc --infile="$f" --outHopsFile=scratch/maxhopscmd_550.txt --outTimeFile=scratch/maxtimecmd_550.txt --outAvgMsgFile=scratch/avgmsgcmd_550.txt"
+done
