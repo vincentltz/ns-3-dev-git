9ffcf8c87e41a12c711f65dfc9458b9243eaed15
Tingzhi Li <liting@flip2.engr.oregonstate.edu>
2015-05-25 10:54:00 -0700

    results

--- build/build-status.py
+++ build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3-dev-topoTestAdhoc-optimized', 'build/scratch/ns3-dev-topoTest-optimized', 'build/scratch/ns3-dev-wifi-simple-adhoc-grid-optimized', 'build/scratch/ns3-dev-wifi-simple-adhoc-optimized']
+ns3_runnable_programs = ['build/scratch/ns3-dev-scratch-simulator-optimized', 'build/scratch/ns3-dev-topoTest-optimized', 'build/scratch/ns3-dev-wifi-simple-adhoc-grid-optimized', 'build/scratch/ns3-dev-wifi-simple-adhoc-optimized', 'build/scratch/ns3-dev-topoTestAdhoc-optimized']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
